cc7a17bad0dc7c5909c15beb173a7b390bee39ba
MGlants <mark@glants.xyz>
2024-05-31 04:30:43 +0300

    feat: add emondada

---
+++ apps/emonoda/ci/latest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import requests
+import json
+import semver
+
+
+# Get the latest version of matchbox
+
+URL = "https://api.github.com/repos/mdevaev/emonoda/tags"
+
+def get_latest(channel):
+    r = requests.get(URL)
+    data = json.loads(r.text)
+    versions = [ semver.Version.parse(release['name'].replace('v', '')) for release in data ]
+    latest_version = max(versions)
+
+    return str(latest_version)
+
+if __name__ == "__main__":
+    import sys
+    channel = sys.argv[1]
+    print(get_latest(channel))
